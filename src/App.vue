0da19e673b1389a9784ae444ed5884276b148d7f
KiwiIT <77931084+KiwiOnIT@users.noreply.github.com>
2022-06-11 16:04:35 +0200

    Initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,166 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+<body>
+<div class="row">
+  <div>
+    <a href="https://github.com/KiwiOnIT" class="home">
+      <img src="@/assets/Watermark.png" height="40" class="homeIMG">
+    </a>
+  </div>
+</div>
+
+<div class="row justify-content-md-center vertical-center d-flex align-items-center min-vh-100 space">
+  <div class="col col-lg-2">
+    <h1 class="title">Salut ! Je m'appelle Anthonin (Kiwi) <img src="https://github.com/SatYu26/SatYu26/raw/master/Assets/Hi.gif" width="30px" style="display:initial;vertical-align:initial;"></h1>
+    <p class="text">Je suis un jeune devloppeur qui touche a quelque language et les utilises pour des petits projets</p>
+  </div>
+  <div class="col-md-auto space">
+    <img src="@/assets/logo.png"  class="logo" width="400" height="400">
+  </div>
+</div>
+
+<h1 class="text-white title-size" style="margin-top : 3.75rem;">Mes compétences</h1>
+<div class="row justify-content-md-center">
+  <div class="box vertical-center d-flex align-items-center">
+    <img class="mx-auto" src="@/assets/Js.png" width="64">
+  </div>
+  <div class="box vertical-center d-flex align-items-center space">
+    <img class="mx-auto" src="@/assets/python.png" width="64">
+  </div>
+  <div class="box vertical-center d-flex align-items-center space">
+    <img class="mx-auto" src="@/assets/html5.png" width="64">
+  </div>
+  <div class="box vertical-center d-flex align-items-center space">
+    <img class="mx-auto" src="@/assets/css3.png" width="64">
+  </div>
+  <div class="box vertical-center d-flex align-items-center space">
+    <img class="mx-auto" src="@/assets/vueJs.png" width="64">
+  </div>
+</div>
+
+<h1 class="text-white title-size" style="margin-top : 10.75rem;">Mes outils</h1>
+<div>
+  <div class="row justify-content-md-center">
+    <div class="box vertical-center d-flex align-items-center">
+      <img class="mx-auto" src="@/assets/git.png" width="64">
+    </div>
+    <div class="box vertical-center d-flex align-items-center space">
+      <img class="mx-auto" src="@/assets/terminal.png" width="64">
+    </div>
+    <div class="box vertical-center d-flex align-items-center space">
+      <img class="mx-auto" src="@/assets/npm.png" width="64">
+    </div>
+    <div class="box vertical-center d-flex align-items-center space">
+      <img class="mx-auto" src="@/assets/nodeJS.png" width="64">
+    </div>
+    <div class="box vertical-center d-flex align-items-center space">
+      <img class="mx-auto" src="@/assets/vsc.png" width="64">
+    </div>
+  </div>
+</div>
+<h1 class="text-white title-size" style="margin-top : 10.75rem;">Mes projets</h1>
+<div class="row justify-content-md-center center" style="margin-top : 5.5rem;">
+  <div class="card" style="width: 18rem;">
+  <div class="card-img card-img-top" style="background-image:url(https://images.ctfassets.net/q33z48p65a6w/7GmTIyrf7kNHSyje5E8mxB/aae3dc9e041425ead15b52ecb3e70bab/how-to-make-a-strong-password.png?fit=thumb&fm=webp&h=502&q=70&w=960);">
+		<div class="overlay">
+			<div class="overlay-content">
+				<a href="https://github.com/KiwiOnIT/Passly">View github</a>
+			</div>
+		</div>
+	</div>
+    <div class="card-body card-content">
+        <h2>Passly</h2>
+        <p>PGenerateur de password en VueJs</p>
+    </div>
+  </div>
+  <div class="card" style="width: 18rem;">
+  <div class="card-img card-img-top" style="background-image:url(https://cdn.discordapp.com/avatars/791364666990264411/ad53e2060fa8c1f07d42bbdb0f3e3b20.png?size=1024);">
+		<div class="overlay">
+			<div class="overlay-content">
+				<a href="https://github.com/KiwiOnIT/Para-Bot">View github</a>
+			</div>
+		</div>
+	</div>
+    <div class="card-body card-content">
+        <h2>Para bot</h2>
+        <p>Bot Discord en DiscordJs v12</p>
+    </div>
+  </div>
+  <div class="card" style="width: 18rem;">
+  <div class="card-img card-img-top" style="background-image:url(https://github.com/KiwiOnIT/flop-website/blob/main/src/assets/logowbg.png?raw=true);">
+		<div class="overlay">
+			<div class="overlay-content">
+				<a href="https://github.com/KiwiOnIT/flop-website">View github</a>
+			</div>
+		</div>
+	</div>
+    <div class="card-body card-content">
+        <h2>Flop website</h2>
+        <p>Site web en VueJs pour Flop Clicker</p>
+    </div>
+  </div>
+  <div class="card" style="width: 18rem;">
+  <div class="card-img card-img-top" style="background-image:url(https://media.istockphoto.com/vectors/rock-scissors-paper-icons-hand-gestures-icons-set-rock-scissors-paper-vector-id1275459573?k=20&m=1275459573&s=612x612&w=0&h=CdoWRxTeXhWN20ShDSfmwjlYDxnG0VjBmrhGYPr2iWw=);">
+		<div class="overlay">
+			<div class="overlay-content">
+				<a href="https://github.com/KiwiOnIT/Pierre-feuille-ciseaux">View github</a>
+			</div>
+		</div>
+	</div>
+    <div class="card-body card-content">
+        <h2>Pierre, Feuille, Ciseaux</h2>
+        <p>Mini jeu codé en Python</p>
+    </div>
+  </div>
+</div>
+<div class="row justify-content-md-center center">
+  <div class="card" style="width: 18rem;">
+  <div class="card-img card-img-top" style="background-image:url(https://media.istockphoto.com/vectors/rope-hanging-loop-vector-id635821612?k=20&m=635821612&s=612x612&w=0&h=7PRgxHAu_oXumYxYk2b8t3dJtBVB5vlvaD5d-uGwXQ0=);">
+		<div class="overlay">
+			<div class="overlay-content">
+				<a href="https://github.com/KiwiOnIT/JeuDuPendu">View github</a>
+			</div>
+		</div>
+	</div>
+    <div class="card-body card-content">
+        <h2>Jeu du Pendu</h2>
+        <p>Mini jeu codé en C++</p>
+    </div>
+  </div>
+  <div class="card" style="width: 18rem;">
+  <div class="card-img card-img-top" style="background-image:url(http://www.learningaboutelectronics.com/images/Guess-a-number-from-1-to-10.png);">
+		<div class="overlay">
+			<div class="overlay-content">
+				<a href="https://github.com/KiwiOnIT/PlusOuMoins">View github</a>
+			</div>
+		</div>
+	</div>
+    <div class="card-body card-content">
+        <h2>Guess the number</h2>
+        <p>Mini jeu codé en C++</p>
+    </div>
+  </div>
+  <div class="card" style="width: 18rem;">
+  <div class="card-img card-img-top" style="background-image:url(https://upload.wikimedia.org/wikipedia/commons/thumb/d/d6/GNOME_Screenshot_icon_2018.svg/1200px-GNOME_Screenshot_icon_2018.svg.png);">
+		<div class="overlay">
+			<div class="overlay-content">
+				<a href="https://github.com/KiwiOnIT/random-screen-generator">View github</a>
+			</div>
+		</div>
+	</div>
+    <div class="card-body card-content">
+        <h2>Generateur de screenshot</h2>
+        <p>Generateur de screenshot en VueJs</p>
+    </div>
+  </div>
+</div>
+<div class="container">
+  <div class="mt-5 border-top">
+    <footer class="py-3 my-4">
+      <p class="text-center text-white">© Made in 2022 by KiwiOnIT</p>
+  </footer>
+  </div>
+</div>
+</body>
 </template>
 
 <style>
@@ -12,19 +169,177 @@
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: #ffffff;
 }
 
-nav {
-  padding: 30px;
+body {
+  background-color: #2b2b2b;
+  color: #ffffff;
 }
 
-nav a {
+.card{
+  background-color: #2b2b2b!important;
+}
+.title {
   font-weight: bold;
-  color: #2c3e50;
+  font-size: 30px;
+  color: #ffffff;
 }
 
-nav a.router-link-exact-active {
-  color: #42b983;
+.text  {
+  font-size: 20px;
+  color: #ffffff93;
+}
+
+.logoDiv {
+  float: right;
+  margin-right: 475px;
+  margin-top: 250px;
+}
+
+.logo {
+  border-radius: 8px;
+  box-shadow: rgba(0, 0, 0, 0.56) 0px 22px 70px 4px;
+}
+
+.home {
+  float: left;
+}
+
+.row {
+  --bs-gutter-x: 0rem !important;
+}
+
+.min-vh-100 {
+    min-height: 90vh!important;
+}
+
+.box {
+  width: 100px;
+  height: 100px;
+  background-color: rgb(50, 50, 50);
+  border-radius: 12px;
+  margin-top: 50px;
+  box-shadow: 0 22px 70px 4px rgb(0 0 0 / 56%);
+}
+
+.space {
+  margin-left: 1% ;
+}
+
+.title-size {
+  font-size: 54px;
+}
+
+.card {
+  width: 300px;
+  display: inline-block;
+  margin: 1rem;
+  border-radius: 4px;
+  box-shadow: 0 22px 70px 4px rgb(0 0 0 / 56%);
+  transition: all 0.2s ease;
+  background: #fff;
+  position: relative;
+  overflow: hidden;
+}
+.card:hover, .card.hover {
+  transform: translateY(-4px);
+  box-shadow: 0 22px 70px 4px rgb(0 0 0 / 56%);
+}
+.card:hover .card-content, .card.hover .card-content {
+  box-shadow: inset 0 3px 0 0 #5ea4cc;
+  border-color: #5ea4cc;
+}
+.card:hover .card-img .overlay, .card.hover .card-img .overlay {
+  background-color: rgba(25, 29, 38, .85);
+  transition: opacity 0.2s ease;
+  opacity: 1;
+}
+.card-img {
+  position: relative;
+  height: 224px;
+  width: 100%;
+  background-color: #fff;
+  transition: opacity 0.2s ease;
+  background-position: center center;
+  background-repeat: no-repeat;
+  background-size: cover;
+}
+.card-img .overlay {
+  position: absolute;
+  left: 0;
+  top: 0;
+  width: 100%;
+  height: 100%;
+  background-color: #fff;
+  opacity: 0;
+}
+.card-img .overlay .overlay-content {
+  line-height: 224px;
+  width: 100%;
+  text-align: center;
+  color: #fff;
+}
+.card-img .overlay .overlay-content a {
+  color: #fff;
+  padding: 0 2rem;
+  display: inline-block;
+  border: 1px solid rgba(255, 255, 255, .4);
+  height: 40px;
+  line-height: 40px;
+  border-radius: 20px;
+  cursor: pointer;
+  text-decoration: none;
+}
+.card-img .overlay .overlay-content a:hover, .card-img .overlay .overlay-content a.hover {
+  background: #5ea4cc;
+  border-color: #5ea4cc;
+}
+.card-content {
+  width: 100%;
+  min-height: 104px;
+  background-color: rgb(50, 50, 50)!important;
+  border-top: 1px solid #181717!important;
+  border-bottom-right-radius: 4px;
+  border-bottom-left-radius: 4px;
+  padding: 1rem 2rem;
+  transition: all 0.2s ease;
+}
+.card-content a {
+  text-decoration: none;
+  color: #202927;
+}
+.card-content h2, .card-content a h2 {
+  font-size: 1rem;
+  font-weight: 500;
+}
+.card-content p, .card-content a p {
+  font-size: 0.8rem;
+  font-weight: 400;
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  color: #ffffffc4;
+}
+@media screen and (max-width: 1280px)
+{
+  .center {
+    margin-left: 25px;
+  }
+  .logo {
+    width: 300px;
+    height: 300px;
+  }
+
+  .title {
+    font-size: 25px;
+  }
+  .text {
+    font-size: 15px;
+  }
+
+  .box {
+    margin-left: 20px;
+  }
 }
 </style>
