688de381c046cd20f2ca993cc149dac40bc78be9
KiwiIT <77931084+KiwiOnIT@users.noreply.github.com>
2022-06-13 22:44:25 +0200

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -228,7 +228,7 @@ body {
   background-color: rgb(50, 50, 50);
   border-radius: 12px;
   margin-top: 50px;
-  
+  box-shadow: rgba(0, 0, 0, 0.25) 0px 14px 28px, rgba(0, 0, 0, 0.22) 0px 10px 10px;
 }
 
 .space {
