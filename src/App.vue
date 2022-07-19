82c028a75bc7642d1686ee51e477544c0e9ec839
KiwiIT <77931084+KiwiOnIT@users.noreply.github.com>
2022-07-19 18:23:42 +0200

    First page review

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,5 @@
 <template>
+
 <About />
 
 <div style="padding: 0rem 10%;">
@@ -37,11 +38,25 @@ body {
 .row {
   --bs-gutter-x: 0rem !important;
 }
+
 .min-vh-100 {
-    min-height: 90vh!important;
+  min-height: 90vh!important;
 }
 
 .space {
   margin-left: 1% ;
+}
+
+::-webkit-scrollbar {
+  width: 8px;
+  height: 8px;
+}
+::-webkit-scrollbar-track {
+  background-color: rgba(0, 0, 0, 0.4);
+  border-radius: 10px;
+}
+::-webkit-scrollbar-thumb {
+  background-color: rgb(50, 50, 50);
+  border-radius: 10px;
 }
 </style>
--- src/components/About.vue
+++ src/components/About.vue
@@ -7,10 +7,10 @@
   </div>
 </div>
 
-<div class="row justify-content-md-center vertical-center d-flex align-items-center min-vh-100 space">
-  <div class="col col-lg-2">
-    <h1 class="title">Salut ! Je m'appelle Anthonin (Kiwi) <img src="https://github.com/SatYu26/SatYu26/raw/master/Assets/Hi.gif" width="30px" style="display:initial;vertical-align:initial;"></h1>
-    <p class="text">Je suis un jeune développeur qui touche à quelques langages et les utilise pour des petits projets</p>
+<div class="row justify-content-md-center vertical-center d-flex align-items-center space min-vh-100">
+  <div class="col col-4 col-lg-4 test">
+    <h1 class="title">Salut !<img src="https://github.com/SatYu26/SatYu26/raw/master/Assets/Hi.gif" width="30px"></h1>
+    <p class="title">Je m'appelle <strong class="title-name">Anthonin (Kiwi)</strong></p>
   </div>
   <div class="col-md-auto space">
     <img src="@/assets/logo.png"  class="logo" width="400" height="400">
@@ -20,19 +20,24 @@
 
 <script>
 export default {
-    name:'AboutView'
+    name:'AboutView',
 }
 </script>
 
 <style scoped>
-.title {
-  font-weight: bold;
-  font-size: 30px;
-  color: #ffffff;
+.about {
+  margin-right: 300px;
 }
-.text  {
-  font-size: 20px;
-  color: #ffffffc9;
+
+.title {
+  font-size: 40px;
+  color: #ffffff;
+  text-align: left;
+}
+
+.title-name {
+  color:aqua;
+  font-weight: bold;
 }
 .logoDiv {
   float: right;
@@ -40,8 +45,8 @@ export default {
   margin-top: 250px;
 }
 .logo {
-  border-radius: 8px;
   box-shadow: rgba(0, 0, 0, 0.25) 0px 14px 28px, rgba(0, 0, 0, 0.22) 0px 10px 10px;
+  border-radius: 16px
 }
 .home {
   float: left;
@@ -49,11 +54,8 @@ export default {
   margin-top: 8px;
 }
 
-@media screen and (max-width: 1280px)
+@media screen and (max-width: 600px)
 {
-  .center {
-    margin-left: 25px;
-  }
   .logo {
     width: 300px;
     height: 300px;
@@ -63,6 +65,16 @@ export default {
   }
   .text {
     font-size: 15px;
+  }
+  .test {
+    width: 50%!important;
+    margin-top: 10vh!important;
+    margin-bottom: 5vh!important;
+    margin-right: auto!important;
+    margin-left: auto!important;
+  }
+  .min-vh-100 {
+    min-height: 0vh!important;
   }
 }
 </style>
